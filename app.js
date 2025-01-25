b16a3fd6108e6644d2230472867b196b04f2149c
spiq85 <syavalval@gmail.com>
2025-01-25 13:26:31 +0700

    feat: integrate to database and create API read

--- app.js
+++ app.js
@@ -39,11 +39,13 @@ app.use(function(err, req, res, next) {
 });
 
 // Set port
-const port = process.env.PORT || 4000;
+const port = process.env.APP_PORT || 4000;
 
 // Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const { pool } = require('../database/db');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
 });
 
 module.exports = router;
